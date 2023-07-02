905973a5b74c20deb3e7730abc3b11da251c9b6c
danielfabiani.dev <danielfabiani.dev@gmail.com>
2023-07-02 13:13:20 -0300

    detail de cada juego realizado

--- client/src/components/detail/Detail.jsx
+++ client/src/components/detail/Detail.jsx
@@ -13,23 +13,26 @@ const Detail = () => {
   }, [dispatch, id]);
 
   const DetailGame = useSelector((state) => state.DetailGame)
-
+  const platforms = DetailGame.platforms.join(', ');
+  const genres = DetailGame.genres.join(', ');
   return (
     <div className={styles.detailContainer}>
 
       <div className={styles.detailImage}>
+        <h1>{DetailGame.name}</h1>
         <img src={DetailGame.image} alt="" />
+        <h3>Platforms: </h3>
+        <p>{platforms}</p>
+        <p>Released:  {DetailGame.released}</p>
+        <a href={DetailGame.website}>Website</a>
       </div>
 
       <div className={styles.detailInfo}>
-        <h1>{DetailGame.name}</h1>
         <h3>Description: </h3>
         <p>{DetailGame.description}</p>
-        <p>{DetailGame.platforms.join(' | ')}</p>
-        <p>{DetailGame.released}</p>
-        <p>{DetailGame.website}</p>
-        <p>{DetailGame.rating}</p>
-        <p>{DetailGame.genres.join(' | ')}</p>
+        <p>Rating: {DetailGame.rating}</p>
+        <p>Genero: {genres}</p>
+        
       </div>
 
     </div>
