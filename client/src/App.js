9af5ee366253e5fa5e886a155cea72cf4d5c24ef
danielfabiani.dev <danielfabiani.dev@gmail.com>
2023-07-17 14:27:21 -0300

    cambio de las rutas para deploy

--- client/src/App.js
+++ client/src/App.js
@@ -8,8 +8,8 @@ import Detail from "./components/detail/Detail";
 import Form from "./views/Form/Form";
 import NameVideogames from "./views/NameVideogames/NameVideogames";
 import axios from "axios";
-//axios.defaults.baseURL = 'https://videogamesspaproject-production.up.railway.app/';
-axios.defaults.baseURL = 'http://localhost:3001/';
+axios.defaults.baseURL = 'https://videogamesspaproject-production.up.railway.app/';
+//axios.defaults.baseURL = 'http://localhost:3001/';
 
 
 function App() {
